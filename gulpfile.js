728ee90997a780dcf6203bec01ce1ede3dfebe58
KotoFeelGood72 <kotenkolove@yandex.ru>
2023-03-01 11:10:27 +0300

    Minify img

--- gulpfile.js
+++ gulpfile.js
@@ -15,8 +15,7 @@ var gulp = require('gulp'),
 	sourcemaps = require('gulp-sourcemaps'),
 	postcss = require('gulp-postcss'),
 	assets = require('postcss-assets'),
-	notify = require('gulp-notify'),
-	webp = require('gulp-webp');
+	notify = require('gulp-notify')
 
 let productionStatus;
 
@@ -24,7 +23,6 @@ let productionStatus;
 var purify = require('gulp-purifycss'),
 	terser = require('gulp-terser'),
 	image = require('gulp-image'),
-	pngquant = require('imagemin-pngquant'),
 	csso = require('gulp-csso');
 
 //plugins for testing
@@ -46,19 +44,7 @@ var assetsDir = 'assets/',
 	outputDir = 'dist/',
 	buildDir = 'build/';
 
-//--------------------------------------webp
-// gulp.task('imgWebp', function () {
-// 	return gulp
-// 		.src(assetsDir + 'i/**/*')
-// 		.pipe(
-// 			webp({
-// 				quality: 80,
-// 			})
-// 		)
-// 		.pipe(gulp.dest(outputDir + 'i/'))
-// 		.pipe(browserSync.stream({ once: true }));
-// });
-//--------------------------------------webp###
+
 
 //----------------------------------------------------Compiling
 gulp.task('pug', function () {
@@ -133,7 +119,7 @@ gulp.task('fontsConvert', function () {
 	return gulp
 		.src([assetsDir + 'fonts/*.woff', assetsDir + 'fonts/*.woff2'])
 		.pipe(cssfont64())
-		// .pipe(gulp.dest(outputDir + 'styles/'))
+		.pipe(gulp.dest(outputDir + 'styles/'))
 		.pipe(browserSync.stream({ once: true }));
 });
 
@@ -147,6 +133,10 @@ gulp.task('imageSync', function () {
 		.pipe(gulp.dest(outputDir + 'i/'))
 		.pipe(browserSync.stream({ once: true }));
 });
+
+
+
+
 
 gulp.task('fontsSync', function () {
 	return gulp
@@ -176,7 +166,6 @@ gulp.task('watch', function () {
 		gulp.series('jsConcatComponents')
 	);
 	gulp.watch(assetsDir + 'i/**/*', gulp.series('imageSync'));
-	// gulp.watch(assetsDir + 'i/**/*', gulp.series('imgWebp'));
 	gulp.watch(
 		assetsDir + 'fonts/**/*',
 		gulp.series('fontsSync', 'fontsConvert')
@@ -187,10 +176,11 @@ gulp.task('watch', function () {
 var plugins = {
 	browserSync: {
 		options: {
-			port: 1337,
+			port: 3030,
 			server: {
 				baseDir: outputDir,
 			},
+			tunnel: '',
 		},
 	},
 };
@@ -228,6 +218,27 @@ gulp.task('imgBuild', function () {
 		)
 		.pipe(gulp.dest(buildDir + 'i/'));
 });
+
+gulp.task('imgMinify', function () {
+	return gulp
+		.src([assetsDir + 'i/**/*', '!' + assetsDir + 'i/sprite/**/*'])
+		.pipe(
+			image({
+				pngquant: true,
+				optipng: false,
+				zopflipng: true,
+				jpegRecompress: false,
+				mozjpeg: true,
+				gifsicle: true,
+				svgo: false,
+				concurrent: 10,
+				quiet: false, // defaults to false
+			})
+		)
+		.pipe(gulp.dest(outputDir + 'i/'));
+});
+
+
 
 //copy sprite.svg
 gulp.task('copySprite', function () {
@@ -368,6 +379,7 @@ let taskArray = {
 			'pug',
 			'sass',
 			// 'imgWebp',
+			'imgMinify',
 			'imageSync',
 			'fontsSync',
 			'fontsConvert',
@@ -396,127 +408,7 @@ let taskArray = {
 gulp.task('default', taskArray['development']);
 gulp.task('build', taskArray['production']);
 
-//--------------------------------- testing
 
-// const puppeteer = require('puppeteer');
-// const fs = require('fs');
-// const PNG = require('pngjs').PNG;
-// const pixelmatch = require('pixelmatch');
 
-// const chromeLauncher = require('chrome-launcher');
-// const http = require('http');
-// const staticN = require('node-static');
-// const path = require('path');
 
-// var beforeDir = 'test/before/',
-// 		afterDir = 'test/after/',
-// 		diffDir = 'test/difference/';
 
-// gulp.task('test-init', function() {
-// 	if (!fs.existsSync('test')){
-// 		fs.mkdirSync('test');
-// 	}
-
-// 	if (!fs.existsSync(beforeDir)){
-// 		fs.mkdirSync(beforeDir);
-// 	}
-
-// 	if (fs.existsSync(beforeDir)){
-// 		fs.readdir(beforeDir, (err, files) => {
-// 			for (const file of files) {
-// 				fs.unlink(path.join(beforeDir, file), err => {});
-// 			}
-// 		});
-// 	}
-
-// 	pageList.map(async function(element, index) {
-// 		const browser = await puppeteer.launch();
-// 		const page = await browser.newPage();
-
-// 		await page.setViewport({ width: initialPageWidth, height: 0 });
-
-// 		await page.goto('http://localhost:1337/' + element + '.html');
-
-// 		await page.screenshot({path: beforeDir + element + '.png', fullPage: true});
-// 		console.log(element + ' page +');
-
-// 		await browser.close();
-// 	})
-// })
-
-// gulp.task('test-compare', function() {
-// 	// make and compare screens
-
-// 	var timeMod = new Date().getTime();
-// 	var clearDir = [diffDir, afterDir, 'test/']
-
-// 	if (!fs.existsSync(afterDir)){
-// 		fs.mkdirSync(afterDir);
-// 	}
-
-// 	if (!fs.existsSync(diffDir)){
-// 		fs.mkdirSync(diffDir);
-// 	}
-
-// 	clearDir.map(function(element, index) {
-// 		if (fs.existsSync(element)){
-// 			fs.readdir(element, (err, files) => {
-// 				for (const file of files) {
-// 					fs.unlink(path.join(element, file), err => {});
-// 				}
-// 			});
-// 		}
-// 	});
-
-// 	function doneReading(img1, img2, pageName) {
-// 		var diff = new PNG({width: img1.width, height: img1.height});
-
-// 		pixelmatch(img1.data, img2.data, diff.data, img1.width, img1.height, {threshold: 0.5});
-
-// 		diff.pack().pipe(fs.createWriteStream(diffDir + pageName + timeMod + '.png'));
-// 		console.log(pageName + ' ---- page compared');
-// 	}
-
-// 	function parse2(element, index, pageName) {
-// 		img2[index] = fs.createReadStream(beforeDir + element + '.png').pipe(new PNG()).on('parsed', function() { doneReading(img1[index], img2[index], element)});
-// 	}
-
-// 	pageList.map(async function(element, index) {
-// 		const browser = await puppeteer.launch();
-// 		const page = await browser.newPage();
-
-// 		await page.setViewport({ width: initialPageWidth, height: 0 });
-
-// 		await page.goto('http://localhost:1337/' + element + '.html');
-
-// 		await page.screenshot({path: afterDir + element + '.png', fullPage: true});
-
-// 		await browser.close();
-
-// 		pageName = element;
-// 		img1[index] = await fs.createReadStream(afterDir + element + '.png').pipe(new PNG()).on('parsed', function() { parse2(element, index)});
-// 	})
-
-// 	// create file in insert list of images
-// 	var imgList = pageList.map(function(file, i) {
-// 		return '<li style="width: 49%; display: inline-block; list-style: none; background-color: #888;"><h2 style="font: 3vw sans-serif; margin: 0; padding: 1em; text-align: center;">' + pageList[i] + '</h2><img style="width: 100%; display: block;" src="difference/' + file + timeMod + '.png"/></li>'
-// 	})
-
-// 	fs.writeFile('test/index_test' + timeMod + '.html', imgList, function (err) {});
-
-// 	// create localserver and run chrome
-// 	var fileServer = new staticN.Server();
-
-// 	http.createServer(function (req, res) {
-// 		req.addListener('end', function () {
-// 				fileServer.serve(req, res);
-// 		}).resume();
-// 	}).listen(8080);
-
-// 	chromeLauncher.launch({
-// 		startingUrl: 'http://localhost:8080/test/index_test' + timeMod + '.html',
-// 		userDataDir: false
-// 	}).then(chrome => {
-// 		console.log(`Chrome debugging port running on ${chrome.port}`);
-// 	});
-// })
