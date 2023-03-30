9ba81f6ea15f2bd3f00daf5ad0cf9ed378d94d33
KotoFeelGood72 <kotenkolove@yandex.ru>
2023-03-30 16:26:16 +0300

    Fix bags

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,7 +17,7 @@ $(document).ready(function ($) {
 	modal();
 	checkSubmenu();
 	rotateCard();
-
+	visibleSeo();
 
 	if(devStatus) {
 		pageWidget(['index']);
@@ -332,6 +332,27 @@ function accordion(title, content) {
 		}
 	});
 };
+
+
+function visibleSeo() {
+  let seoBlocks = document.querySelectorAll(".seo-block--w");
+
+  Array.from(seoBlocks).forEach((seoBlock) => {
+    let loadMoreButton = seoBlock.querySelector(".loadmore_btn");
+    let smallBlock = seoBlock.querySelector(".seo-block");
+    smallBlock.classList.remove("visible");
+
+    loadMoreButton.addEventListener("click", function () {
+      if (smallBlock.classList.contains("visible")) {
+        smallBlock.classList.remove("visible");
+        loadMoreButton.querySelector("button").innerHTML = "Показать еще";
+      } else {
+        smallBlock.classList.add("visible");
+        loadMoreButton.querySelector("button").innerHTML = "Скрыть";
+      }
+    });
+  });
+}
 
 
 
