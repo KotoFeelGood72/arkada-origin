b28cccb8a704ecb219b2f78ce4875bc34fdbdc22
KotoFeelGood72 <kotenkolove@yandex.ru>
2023-04-27 15:50:50 +0300

    dd

--- assets/js/main.js
+++ assets/js/main.js
@@ -150,14 +150,14 @@ $(document).ready(function() {
 
 
 
-$(document).ready(function()  {
+// $(document).ready(function()  {
 
-	var inputsTel = document.querySelectorAll('input[type="tel"]');
-	Inputmask({
-		"mask": "+7 (999) 999-99-99",
-		showMaskOnHover: false
-	}).mask(inputsTel);
-})
+// 	var inputsTel = document.querySelectorAll('input[type="tel"]');
+// 	Inputmask({
+// 		"mask": "+7 (999) 999-99-99",
+// 		showMaskOnHover: false
+// 	}).mask(inputsTel);
+// })
 
 
 
